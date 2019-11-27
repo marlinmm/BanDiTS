c824a86f2077ac25696c482e7c864e421b917ec0
jziemer1996 <jz199623@gmail.com>
2019-11-27 10:29:01 +0100

    add plot

--- GEO419_South_Africa/test_file_2.py
+++ GEO419_South_Africa/test_file_2.py
@@ -1,1 +1,50 @@
 #test3eteatda
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 14:54:54 2019
+
+@author: jonas
+"""
+
+
+import matplotlib.pyplot as plt
+import pylab as plt
+import numpy as np
+import pandas as pd
+import skimage
+import skimage.filters as filters
+from skimage import io
+from PIL import Image
+
+
+"""
+df = pd.read_csv("C:/Users/jonas/Documents/Studium/Master/1. Semester/Vorlesungsmitschriften/GEO419 - Pythonprogrammierung Habermeyer/Ziemer_uebungen/Python/data/testpivot3EEVH.csv")
+
+print(df.head())
+
+print(df.tail())
+
+x = df['Band']
+y = df['VH']
+plt.xlabel('Time Series'); plt.ylabel('VH')
+plt.plot(x,y)
+
+"""
+
+id = 1
+
+filenames = ["VH", "VV"]
+
+filename = "C:/Users/jonas/Documents/Studium/Master/1. Semester/Vorlesungsmitschriften/GEO419 - Pythonprogrammierung Habermeyer/Ziemer_uebungen/Python/data/radar/Subset_" + filenames[id] + ".tif"
+
+
+img = skimage.io.imread(filename)
+
+
+plt.plot(img)
+
+"""
+I = plt.imread("C:/Users/jonas/Desktop/Subset_VV.tif")
+"""
