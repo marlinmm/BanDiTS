019ac2a81dfd5fcaf24158397da453568fe02cf5
marlinmm <marlinmueller.mm@gmail.com>
2019-12-14 16:36:43 +0100

    first try - for loop through subset - 9 seconds for 1k pixel

--- GEO419_South_Africa/numpy_array_test.py
+++ GEO419_South_Africa/numpy_array_test.py
@@ -15,15 +15,22 @@ from datetime import datetime
 start_time = datetime.now()
 
 numpy.set_printoptions(threshold=sys.maxsize)
-im = io.imread("C:/Users/marli/Desktop/GEO402_Testdaten/S1A_VH_Agulhas_50m_selected_bands_VH.tif")
+im = io.imread("C:/Users/marli/Desktop/GEO402_Testdaten/S1A_VH_Agulhas_50m_selected_bands_VH_subset.tif")
 print(im.shape)
+result_list = []
+time_list = []
 
-test_list = im[1, 2, :]
-test_list = test_list[test_list != -99]
+for y in range(0, 122):
+    for x in range(0,117):
+        time_list = im[x, y, :]
+        time_list = time_list[time_list != -99]
+        result_list.append(time_list)
+        x += 1
+    y += 1
+
+
+print(time_list)
+print(result_list)
+
 mid_time = datetime.now()
-
-
-plt.plot(test_list)
-plt.show()
 print("mid-time = ", mid_time-start_time, "Hr:min:sec")
-print(type(im))
