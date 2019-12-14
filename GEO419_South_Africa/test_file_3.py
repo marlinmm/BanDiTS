466d9dfe4c0946b4a6c9391f6ed0cf0e4e375b51
marlinmm <marlinmueller.mm@gmail.com>
2019-12-14 18:24:41 +0100

    added some changes

--- GEO419_South_Africa/test_file_3.py
+++ GEO419_South_Africa/test_file_3.py
@@ -10,17 +10,15 @@ start_time = datetime.now()
 def geek_fun(a):
     # Returning the sum of elements at start index and at last index
     # inout array
-    return (a[1])
+    return (a[0])
 
 # Unser Rasterbild als 2D-Array
-arr = geek.array([[1, 4, 5],
-                  [6, 2, 8],
-                  [9, 3, 5]])
+arr = geek.array([[[1,2,3], [4,5,6], [7,999,9]], [[1,2,3], [4,5,6], [7,8,9]], [[1,2,3], [4,5,6], [7,8,9]]])
 
-print("axis=0 : ", geek.apply_along_axis(geek_fun, 0, arr))
+print("axis=0 : ", geek.apply_along_axis(geek_fun, 2, arr))
 print("\n")
 
-print("axis=1 : ", geek.apply_along_axis(geek_fun, 1, arr))
+#print("axis=1 : ", geek.apply_along_axis(geek_fun, 1, arr))
 
 end_time = datetime.now()
 print(end_time-start_time, "seconds")
