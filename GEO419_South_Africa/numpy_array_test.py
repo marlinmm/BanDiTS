afdb702d49636ea2cbd72384d198fb146a8f7f82
marlinmm <marlinmueller.mm@gmail.com>
2019-12-14 18:24:27 +0100

    changed a bunch of stuff to try multithreading

--- GEO419_South_Africa/numpy_array_test.py
+++ GEO419_South_Africa/numpy_array_test.py
@@ -4,36 +4,38 @@ File to test numpy array and stuff like that
 import sys
 import numpy
 import os
-from glob import glob
-import matplotlib.pyplot as plt
-import earthpy as et
-import earthpy.spatial as es
-import earthpy.plot as ep
 from skimage import io
 from datetime import datetime
+import pathos.multiprocessing as mp
 
 start_time = datetime.now()
 
 numpy.set_printoptions(threshold=sys.maxsize)
-#im = io.imread("C:/Users/marli/Desktop/GEO402_Testdaten/S1A_VH_Agulhas_50m_selected_bands_VH_subset.tif")
+im = io.imread("C:/Users/marli/Desktop/GEO402_Testdaten/S1A_VH_Agulhas_50m_selected_bands_VH_subset.tif")
+#im = io.imread("C:/Users/jz199/Desktop/S1A_VH_Agulhas_50m_selected_bands_VH.tif")
 
-im = io.imread("C:/Users/jz199/Desktop/S1A_VH_Agulhas_50m_selected_bands_VH.tif")
+shape = im.shape
+print(shape)
 
-print(im.shape)
 result_list = []
 time_list = []
 
-for y in range(0, 122):
-    for x in range(0,117):
-        time_list = im[x, y, :]
-        time_list = time_list[time_list != -99]
-        result_list.append(time_list)
-        x += 1
-    y += 1
+def for_loop_pixel():
+    for y in range(0, 122):
+        for x in range(0,117):
+            time_list = im[x, y, :]
+            time_list = time_list[time_list != -99]
+            result_list.append(time_list)
+            x += 1
+        y += 1
+    print(result_list)
+    mid_time = datetime.now()
+    print("mid-time = ", mid_time - start_time, "Hr:min:sec")
+
+for_loop_pixel()
 
 
-print(time_list)
-print(result_list)
+print(shape)
 
 mid_time = datetime.now()
 print("mid-time = ", mid_time-start_time, "Hr:min:sec")
