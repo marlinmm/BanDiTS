856eaa48cabcd4cbab8cf4cb3ea249c255cd1378
marlinmm <marlinmueller.mm@gmail.com>
2020-01-21 12:32:41 +0100

    changed func name

--- GEO419_South_Africa/import_arr.py
+++ GEO419_South_Africa/import_arr.py
@@ -1,7 +1,7 @@
 from osgeo import gdal
 from osgeo.gdalconst import GA_ReadOnly
 
-def import_array():
+def gdal_array():
     #filename = 'C:/Users/jonas/Desktop/S1A_VH_Agulhas_50m_selected_bands_VH_subset.tif'
     filename = "C:/Users/marli/Desktop/GEO402_Testdaten/S1A_VH_Agulhas_50m_selected_bands_VH_subset_big.tif"
 
@@ -9,7 +9,5 @@ def import_array():
 
     arr = ras.ReadAsArray()
     return(arr)
-    #print(arr)
 
-abc = import_array()
-print(abc)
+
