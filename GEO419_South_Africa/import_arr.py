bc1d7d18908a29a028b314e0db9491927b1a99b7
marlinmm <marlinmueller.mm@gmail.com>
2020-01-21 15:41:53 +0100

    made filename variable outside of function

--- GEO419_South_Africa/import_arr.py
+++ GEO419_South_Africa/import_arr.py
@@ -1,10 +1,12 @@
 from osgeo import gdal
 from osgeo.gdalconst import GA_ReadOnly
 
+filename = "C:/Users/marli/Desktop/GEO402_Testdaten/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif"
+
 def gdal_array():
     #filename = 'C:/Users/jonas/Desktop/S1A_VH_Agulhas_50m_selected_bands_VH_subset.tif'
-    filename = "C:/Users/marli/Desktop/GEO402_Testdaten/S1A_VH_Agulhas_50m_selected_bands_VH_subset_big.tif"
-
+    #filename = "C:/Users/marli/Desktop/GEO402_Testdaten/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif"
+    #filename = "C:/Users/marli/Desktop/GEO402_Testdaten/S1A_VV_Agulhas_50m_selected_bands_VV.tif"
     ras = gdal.Open(filename, GA_ReadOnly)
 
     arr = ras.ReadAsArray()
