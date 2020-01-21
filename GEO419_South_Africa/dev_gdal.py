d2f078c95cf4cd5625d368f162aad5b190bc8f41
marlinmm <marlinmueller.mm@gmail.com>
2020-01-21 12:07:25 +0100

    made into function

--- GEO419_South_Africa/dev_gdal.py
+++ GEO419_South_Africa/dev_gdal.py
@@ -1,8 +1,15 @@
 from osgeo import gdal
 from osgeo.gdalconst import GA_ReadOnly
 
-filename = 'C:/Users/jonas/Desktop/S1A_VH_Agulhas_50m_selected_bands_VH_subset.tif'
+def import_array():
+    #filename = 'C:/Users/jonas/Desktop/S1A_VH_Agulhas_50m_selected_bands_VH_subset.tif'
+    filename = "C:/Users/marli/Desktop/GEO402_Testdaten/S1A_VH_Agulhas_50m_selected_bands_VH_subset_big.tif"
 
-ras = gdal.Open(filename, GA_ReadOnly)
+    ras = gdal.Open(filename, GA_ReadOnly)
 
-arr = ras.ReadAsArray()
+    arr = ras.ReadAsArray()
+    return(arr)
+    #print(arr)
+
+abc = import_array()
+print(abc)
