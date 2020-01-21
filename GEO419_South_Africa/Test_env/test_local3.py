75f74bb7719ebab35a9dfdc3653981edbe996017
marlinmm <marlinmueller.mm@gmail.com>
2020-01-21 12:31:57 +0100

    first combination file (semi final°°°°°°°°°°°°°°°°°°°°°)

---
+++ GEO419_South_Africa/Test_env/test_local3.py
@@ -0,0 +1,21 @@
+from multiprocessing import Pool, TimeoutError
+import time
+import os
+import sys
+import os
+from datetime import datetime
+import numpy as np
+import pathos.multiprocessing as mp
+from pathos import multiprocessing
+from multiprocessing import Process
+from skimage import io
+import pylab as plt
+
+def f(x):
+    return x*x
+
+if __name__ == '__main__':
+    pool = multiprocessing.Pool(mp.cpu_count())            # start 4 worker processes
+
+    # print "[0, 1, 4,..., 81]"
+    print(pool.map(f, range(100000000)))
---
+++ GEO419_South_Africa/merge_concatenate.py
@@ -0,0 +1,5 @@
+from GEO419_South_Africa import import_arr, apply_along_axis
+
+arr = import_arr.gdal_array()
+#print(arr)
+
