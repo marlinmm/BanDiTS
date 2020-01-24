631df1cefb0b0be429a0d68ddee98e75ec6e3366
marlinmm <marlinmueller.mm@gmail.com>
2020-01-24 17:29:41 +0100

    changed variable names to work with main

--- GEO419_South_Africa/export_arr.py
+++ GEO419_South_Africa/export_arr.py
@@ -1,8 +1,8 @@
 import rasterio as rio
 from GEO419_South_Africa import import_arr
 
-def out_array():
-    with rio.open(import_arr.filename) as src:
+def out_array(outname, arr, input_file):
+    with rio.open(input_file) as src:
         ras_data = src.read()
         ras_meta = src.profile
 
@@ -10,5 +10,5 @@ def out_array():
     ras_meta['dtype'] = "float32"
     ras_meta['nodata'] = -99
 
-    with rio.open("C:/Users/marli/Desktop/GEO402_Testdaten/AAA_output/test1.tif", 'w', **ras_meta) as dst:
-        dst.write(numpy_array, 1)
+    with rio.open(outname, 'w', **ras_meta) as dst:
+        dst.write(arr, 1)
