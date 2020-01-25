7d0749ece62baeb7c2937adb73374851163370a5
jziemer1996 <jz199623@gmail.com>
2020-01-25 10:20:48 +0100

    added Jonas path

--- GEO419_South_Africa/merge_concatenate.py
+++ GEO419_South_Africa/merge_concatenate.py
@@ -1,25 +1,25 @@
 #import os
 import numpy as np
-from GEO419_South_Africa import import_arr, apply_along_axis, export_arr
+from GEO419_South_Africa import import_arr, apply_along_axis, export_arr, function
 from pathos import multiprocessing as mp
 
 
 def main():
     # Input Folder Marlin:
-    input_folder = "C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/"
+    # input_folder = "C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/"
     # Input Folder Jonas:
-    # input_folder = "C:/Users/jz199/Documents/Studium/Master/1. Semester/Vorlesungsmitschriften/GEO419 - Pythonprogrammierung Habermeyer/GEO402_Testdaten/"
+    input_folder = "C:/Users/jz199/Documents/Studium/Master/1. Semester/Vorlesungsmitschriften/GEO419 - Pythonprogrammierung Habermeyer/GEO402_Testdaten/"
 
     # Input file name
-    input_filename = "S1A_VH_Agulhas_50m_selected_bands_VH.tif"
+    input_filename = "S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif"
 
     # Output Folder Marlin:
-    output_folder = "C:/Users/marli/Desktop/GEO402_Testdaten/AAA_output/"
+    # output_folder = "C:/Users/marli/Desktop/GEO402_Testdaten/AAA_output/"
     # Output Folder Jonas:
-    #output_folder = ""
+    output_folder = "C:/Users/jz199/Documents/Studium/Master/1. Semester/Vorlesungsmitschriften/GEO419 - Pythonprogrammierung Habermeyer/GEO402_Output/"
 
     # Output File Name:
-    output_file = "test_original_mean.tif"
+    output_file = "test_original_maximum.tif"
 
     ######################   NO USER INPUT BEYOND THIS POINT   ###############################
 
@@ -38,30 +38,9 @@ def main():
     # arr: full size numpy array 3D XxYxZ 200x300x100
     arr = import_arr.rio_array(input_file)
     #arr[arr == -99] = np.nan ## trying to loose -99 values cause it corrupts output map
-    result = apply_along_axis.parallel_apply_along_axis(func1d=mean, arr=arr, axis=0, cores=mp.cpu_count())
+    result = apply_along_axis.parallel_apply_along_axis(func1d=function.maximum, arr=arr, axis=0, cores=mp.cpu_count())
     export_arr.out_array(outname=outname, arr=result, input_file = input_file)
 
-
-
-# func1d: functions to be applied on 1D array
-def quantile(arr1d, percentile=0.5):
-    import numpy as np
-    return np.quantile(arr1d, percentile)
-
-
-def minimum(arr1d):
-    import numpy as np
-    return np.min(arr1d)
-
-
-def maximum(arr1d):
-    import numpy as np
-    return np.max(arr1d)
-
-
-def mean(arr1d):
-    import numpy as np
-    return np.mean(arr1d)
 
 
 # main func
