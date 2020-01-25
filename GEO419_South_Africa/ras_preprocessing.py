197eef791f1e26b167e9c2185a1a2e77aadfba48
marlinmm <marlinmueller.mm@gmail.com>
2020-01-25 18:20:41 +0100

    implemented main preprocessing file to delete layers with only -99 values

--- GEO419_South_Africa/ras_preprocessing.py
+++ GEO419_South_Africa/ras_preprocessing.py
@@ -1,5 +1,15 @@
-import numpy as np
-from GEO419_South_Africa import apply_along_axis, import_arr
+from GEO419_South_Africa import import_arr
 
 
-
+def preprocessing(input_file):
+    import numpy as np
+    arr = import_arr.rio_array(input_file)
+    y_axis_len = arr.shape[1]
+    x_axis_len = arr.shape[2]
+    rotate_arr = arr
+    rotate_arr = np.rollaxis(rotate_arr, 2)
+    rotate_arr = np.rollaxis(rotate_arr, 2)
+    rotate_arr = rotate_arr[y_axis_len//2][x_axis_len//2]
+    no_data = np.where(rotate_arr == -99.        )
+    arr = np.delete(arr, no_data[0], 0)
+    return arr
