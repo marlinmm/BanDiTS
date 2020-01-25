1225bd9d0ac277bc3147a5ed0c878fd5769621f7
marlinmm <marlinmueller.mm@gmail.com>
2020-01-25 18:43:27 +0100

    added data type variable to export function

--- GEO419_South_Africa/export_arr.py
+++ GEO419_South_Africa/export_arr.py
@@ -1,14 +1,14 @@
 import rasterio as rio
 
 
-def out_array(outname, arr, input_file):
+def out_array(outname, arr, input_file, dtype):
     with rio.open(input_file) as src:
         ras_meta = src.profile
         corr_count = {'count': 1}
         ras_meta.update(corr_count)
 
     # make any necessary changes to raster properties, e.g.:
-    ras_meta['dtype'] = "float32"
+    ras_meta['dtype'] = dtype
     #ras_meta['nodata'] = -99
 
     with rio.open(outname, 'w', **ras_meta) as dst:
