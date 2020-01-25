ac2ad1c7ba141e2cd58adf2b00022c240c4ccc1d
marlinmm <marlinmueller.mm@gmail.com>
2020-01-25 18:06:22 +0100

    deleted NaN compatibility

--- GEO419_South_Africa/function.py
+++ GEO419_South_Africa/function.py
@@ -3,19 +3,19 @@ import numpy as np
 # func1d: functions to be applied on 1D array
 def quantile(arr1d, percentile=0.5):
     import numpy as np
-    return np.nanpercentile(arr1d, percentile)
+    return np.percentile(arr1d, percentile)
 
 
 def minimum(arr1d):
     import numpy as np
-    return np.nanmin(arr1d)
+    return np.min(arr1d)
 
 
 def maximum(arr1d):
     import numpy as np
-    return np.nanmax(arr1d)
+    return np.max(arr1d)
 
 
 def mean(arr1d):
     import numpy as np
-    return np.nanmean(arr1d)
+    return np.mean(arr1d)
