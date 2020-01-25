b34db86de093c49a7ce881f284b4bfd8e3f2c536
marlinmm <marlinmueller.mm@gmail.com>
2020-01-25 17:43:32 +0100

    made functions NaN compatible

--- GEO419_South_Africa/function.py
+++ GEO419_South_Africa/function.py
@@ -3,19 +3,19 @@ import numpy as np
 # func1d: functions to be applied on 1D array
 def quantile(arr1d, percentile=0.5):
     import numpy as np
-    return np.quantile(arr1d, percentile)
+    return np.nanpercentile(arr1d, percentile)
 
 
 def minimum(arr1d):
     import numpy as np
-    return np.min(arr1d)
+    return np.nanmin(arr1d)
 
 
 def maximum(arr1d):
     import numpy as np
-    return np.max(arr1d)
+    return np.nanmax(arr1d)
 
 
 def mean(arr1d):
     import numpy as np
-    return np.mean(arr1d)
+    return np.nanmean(arr1d)
