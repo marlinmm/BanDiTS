cec7069890042e89dece319bfff11028e3f67ee8
marlinmm <marlinmueller.mm@gmail.com>
2020-01-26 22:14:04 +0100

    added new test file

---
+++ GEO419_South_Africa/Test_env/new_function_test.py
@@ -0,0 +1,19 @@
+# def split_list(alist, wanted_parts=1):
+#     length = len(alist)
+#     return [alist[i*length // wanted_parts: (i+1)*length // wanted_parts]
+#              for i in range(wanted_parts)]
+#
+# A = [0,1,2,3,4,5,6,7,8,9]
+#
+# print(split_list(A, wanted_parts=1))
+# print(split_list(A, wanted_parts=2))
+# print(split_list(A, wanted_parts=8))
+
+
+
+l = [[[[[], 1.3519724267018103], 2.181101640389802], 0.9728476171745472], -0.1378491506318585]
+
+print(l[1])
+print(l[0][1])
+print(l[0][0][1])
+print(l[0][0][0][1])
