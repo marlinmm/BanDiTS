c9640801df876020eaa2c74b2e7ca114e5d9cfc8
marlinmm <marlinmueller.mm@gmail.com>
2020-01-29 14:49:44 +0100

    adapted func to work with maask (still needs work)

--- GEO419_South_Africa/export_arr.py
+++ GEO419_South_Africa/export_arr.py
@@ -4,12 +4,27 @@ import rasterio as rio
 def out_array(outname, arr, input_file, dtype):
     with rio.open(input_file) as src:
         ras_meta = src.profile
-        corr_count = {'count': 1}
-        ras_meta.update(corr_count)
+        #corr_count = {'count': 119}
+        #ras_meta.update(corr_count)
 
     # make any necessary changes to raster properties, e.g.:
     ras_meta['dtype'] = dtype
     #ras_meta['nodata'] = -99
 
     with rio.open(outname, 'w', **ras_meta) as dst:
-        dst.write(arr, 1)
+        dst.write(arr)
+
+
+#####Original File###########
+# def out_array(outname, arr, input_file, dtype):
+#     with rio.open(input_file) as src:
+#         ras_meta = src.profile
+#         corr_count = {'count': 1}
+#         ras_meta.update(corr_count)
+#
+#     # make any necessary changes to raster properties, e.g.:
+#     ras_meta['dtype'] = dtype
+#     #ras_meta['nodata'] = -99
+#
+#     with rio.open(outname, 'w', **ras_meta) as dst:
+#         dst.write(arr, 1)
