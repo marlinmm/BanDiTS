fec1f1884323c62ea5d680ad39ab879c72e8bc46
marlinmm <marlinmueller.mm@gmail.com>
2020-01-29 15:12:13 +0100

    corrected export file

--- GEO419_South_Africa/export_arr.py
+++ GEO419_South_Africa/export_arr.py
@@ -4,15 +4,15 @@ import rasterio as rio
 def out_array(outname, arr, input_file, dtype):
     with rio.open(input_file) as src:
         ras_meta = src.profile
-        #corr_count = {'count': 119}
-        #ras_meta.update(corr_count)
+        corr_count = {'count': 1}
+        ras_meta.update(corr_count)
 
     # make any necessary changes to raster properties, e.g.:
     ras_meta['dtype'] = dtype
     #ras_meta['nodata'] = -99
 
     with rio.open(outname, 'w', **ras_meta) as dst:
-        dst.write(arr)
+        dst.write(arr, 1)
 
 
 #####Original File###########
