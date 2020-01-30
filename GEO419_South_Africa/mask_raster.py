166f2a110d10ddb5be486bd9d4b742359cc9a58d
marlinmm <marlinmueller.mm@gmail.com>
2020-01-30 17:36:59 +0100

    added metadata update to output

--- GEO419_South_Africa/mask_raster.py
+++ GEO419_South_Africa/mask_raster.py
@@ -1,5 +1,5 @@
 import fiona
-import rasterio
+import rasterio as rio
 import rasterio.mask
 from GEO419_South_Africa import export_arr
 
@@ -8,8 +8,14 @@ def mask_raster_test(outname):
     shapefile = fiona.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Shapes/fire_feb_2017_subset_reproj.shp", "r")
     shapes = [feature["geometry"] for feature in shapefile]
     for i in range(0, len(shapes)):
-        with rasterio.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif") as src:
+        with rio.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif") as src:
             out_image, out_transform = rasterio.mask.mask(src, [shapes[i]], crop=True)
-            # out_meta = src.meta
-            export_arr.out_array(outname=outname + i, arr=out_image, input_file="C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif", dtype="float32")
+            ras_meta = src.profile
+            ras_meta.update({"driver": "GTiff",
+                             "height": out_image.shape[1],
+                             "width": out_image.shape[2],
+                             "transform": out_transform})
+            # export_arr.out_array(outname=outname + i, arr=out_image, input_file="C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif", dtype="float32")
+            with rio.open(outname, 'w', **ras_meta) as dst:
+                dst.write(out_image, 1)
     #return out_image
