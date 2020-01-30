843c1b3d5772fd4b1bdf03610d825fc0beb86bd8
marlinmm <marlinmueller.mm@gmail.com>
2020-01-30 09:46:37 +0100

    changed some values and added slope vs slope minimum function

--- GEO419_South_Africa/function.py
+++ GEO419_South_Africa/function.py
@@ -29,7 +29,7 @@ def stdev(arr1d):
 
 def improved_stdev(arr1d):
     import numpy as np
-    diff1 = np.mean(arr1d)-3*np.std(arr1d)
+    diff1 = np.mean(arr1d)-2.5*np.std(arr1d)
     if np.min(arr1d) < diff1:
         return 1
     else:
@@ -38,7 +38,7 @@ def improved_stdev(arr1d):
 
 def threshold(arr1d):
     import numpy as np
-    if np.min(arr1d) < -20:
+    if np.min(arr1d) < -21:
         return 1
     else:
         return 0
@@ -47,8 +47,8 @@ def threshold(arr1d):
 def combined(arr1d):
     import numpy as np
     diff = np.max(arr1d) - np.min(arr1d)
-    sigma = np.mean(arr1d) - 2.5 * np.std(arr1d)
-    if diff >= 7:
+    sigma = np.mean(arr1d) - 3 * np.std(arr1d)
+    if diff >= 6:
         if np.min(arr1d) < sigma:
             return 1
         else:
@@ -60,9 +60,9 @@ def combined(arr1d):
 def amplitude_if_test(arr1d):
     import numpy as np
     diff = np.max(arr1d) - np.min(arr1d)
-    if diff < 9:
+    if diff < 8:
         return 0
-    if diff >= 9:
+    if diff >= 8:
         return 1
 
 
@@ -131,4 +131,29 @@ def slope_vs_slope(arr1d):
     #### UNFERTIG ####
 
 
-#def mean_2sigma():
+def slope_minimum(arr1d):
+    import numpy as np
+    from scipy import stats
+    time_series = arr1d
+    arr_shape = arr1d.shape[0]
+    time_series_index = np.indices((arr_shape,))[0]
+
+    # internal function to split time series in n sub time series
+    def split_list(alist, wanted_parts=1):          # based on: https://stackoverflow.com/a/752562
+        length = len(alist)
+        return [alist[i * length // wanted_parts: (i + 1) * length // wanted_parts]
+                for i in range(wanted_parts)]
+
+    # split time series and list of time series indices in 4 subarrays
+    time_series_split = split_list(time_series, wanted_parts=5)
+    time_series_index_split = split_list(time_series_index, wanted_parts=5)
+    slope_list = []
+
+    # calculate linear regression for each time series subarray
+    for i in range (0,len(time_series_index_split)):
+        slope, intercept, r_value, p_value, std_err = stats.linregress(time_series_split[i],time_series_index_split[i])
+        i +=1
+        slope_list = [slope_list, slope] 
+
+    slope_min = min(slope_list[0][0][0][0][1], slope_list[0][0][0][1], slope_list[0][0][1], slope_list[0][1], slope_list[1])
+    return slope_min
