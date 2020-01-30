3cfd91497a2cf21ec9bba66c17a0a1930b7d1998
marlinmm <marlinmueller.mm@gmail.com>
2020-01-30 16:54:17 +0100

    removed slope min function

--- GEO419_South_Africa/function.py
+++ GEO419_South_Africa/function.py
@@ -128,32 +128,5 @@ def slope_vs_slope(arr1d):
     # else:
     #     x = 100
     return x
-    #### UNFERTIG ####
 
 
-def slope_minimum(arr1d):
-    import numpy as np
-    from scipy import stats
-    time_series = arr1d
-    arr_shape = arr1d.shape[0]
-    time_series_index = np.indices((arr_shape,))[0]
-
-    # internal function to split time series in n sub time series
-    def split_list(alist, wanted_parts=1):          # based on: https://stackoverflow.com/a/752562
-        length = len(alist)
-        return [alist[i * length // wanted_parts: (i + 1) * length // wanted_parts]
-                for i in range(wanted_parts)]
-
-    # split time series and list of time series indices in 4 subarrays
-    time_series_split = split_list(time_series, wanted_parts=5)
-    time_series_index_split = split_list(time_series_index, wanted_parts=5)
-    slope_list = []
-
-    # calculate linear regression for each time series subarray
-    for i in range (0,len(time_series_index_split)):
-        slope, intercept, r_value, p_value, std_err = stats.linregress(time_series_split[i],time_series_index_split[i])
-        i +=1
-        slope_list = [slope_list, slope] 
-
-    slope_min = min(slope_list[0][0][0][0][1], slope_list[0][0][0][1], slope_list[0][0][1], slope_list[0][1], slope_list[1])
-    return slope_min
