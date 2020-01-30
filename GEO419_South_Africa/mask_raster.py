39c9bd16ba0735c50fea4166829ccebbd70674c6
marlinmm <marlinmueller.mm@gmail.com>
2020-01-30 17:04:19 +0100

    changed mask raster function from test file

--- GEO419_South_Africa/mask_raster.py
+++ GEO419_South_Africa/mask_raster.py
@@ -1,16 +1,15 @@
-import rasterio as rio
+import fiona
+import rasterio
 import rasterio.mask
+from GEO419_South_Africa import export_arr
 
-def raster_mask_func(raster, shape, output_folder):
-    out_image, out_transform = rio.mask.mask(raster, shape, crop=True)
-    out_meta = raster.meta
 
-    out_meta.update({"driver": "GTiff",
-                     "height": out_image.shape[1],
-                     "width": out_image.shape[2],
-                     "transform": out_transform})
-    print(out_image)
-    #out_meta['nodata'] = None
+def mask_raster_test(outname):
+    shapefile = fiona.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Shapes/fire_feb_2017_subset_reproj.shp", "r")
+    shapes = [feature["geometry"] for feature in shapefile]
+    for i in range(0, len(shapes)):
+        with rasterio.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif") as src:
+            out_image, out_transform = rasterio.mask.mask(src, [shapes[i]], crop=True)
+            # out_meta = src.meta
+            export_arr.out_array(outname=outname + i, arr=out_image, input_file="C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif", dtype="float32")
     #return out_image
-    #with rio.open(output_folder + "masked.tif", "w", **out_meta) as dest:
-        #dest.write(out_image)
