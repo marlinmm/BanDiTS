dffce99be1551c83841ce7f23edcc8a6308ee37d
marlinmm <marlinmueller.mm@gmail.com>
2020-02-02 00:30:07 +0100

    minor changes to work with new file

--- GEO419_South_Africa/mask_main_marlin.py
+++ GEO419_South_Africa/mask_main_marlin.py
@@ -6,28 +6,6 @@ from datetime import datetime
 start_time = datetime.now()
 
 def main():
-    # # Input Folder Marlin:
-    # raster_folder = "C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/"
-    # shape_folder = "C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Shapes/"
-    # # Input Folder Jonas:
-    # # raster_folder = "C:/Users/jz199/Documents/Studium/Master/1. Semester/Vorlesungsmitschriften/GEO419 - Pythonprogrammierung Habermeyer/GEO402_Testdaten/"
-    #
-    # # Input file name
-    # raster_filename = "S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif"
-    # shape_filename = "fire_feb_2017_subset_reproj.shp"
-    #
-    #
-    # # Output Folder Marlin:
-    # output_folder = "C:/Users/marli/Desktop/GEO402_Testdaten/AAA_output/"
-    # # Output Folder Jonas:
-    # #output_folder = ""
-    #
-    # # Output File Name:
-    # output_file = raster_filename + "_mask_test_test1.tif"
-    #
-    # ######################   NO USER INPUT BEYOND THIS POINT   ###############################
-
-
     ###################################     INPUT    ########################################
 
     # Input Folder Marlin:
@@ -49,7 +27,7 @@ def main():
     # output_folder = "C:/Users/jz199/Documents/Studium/Master/1. Semester\Vorlesungsmitschriften/GEO419 - Pythonprogrammierung Habermeyer/GEO402_Output/"
 
     # Output File Name:
-    output_file = raster_filename + "_slope_slope_min.tif"
+    output_file = raster_filename + "cleaned.tif"
 
     ######################   NO USER INPUT BEYOND THIS POINT   ###############################
 
@@ -94,9 +72,10 @@ def main():
     #result = tes_local_2.mask_raster_test(outname)
     #export_arr.out_array(outname=outname, arr=result, input_file=input_raster, dtype="float32")
 
-    result = apply_along_axis.parallel_apply_along_axis(func1d=function.slope_minimum, arr=arr, axis=0, cores=mp.cpu_count())
-    dtype = type(result[0][0])
-    export_arr.out_array(outname=outname, arr=result, input_file=input_raster, dtype=dtype)
+    #result = apply_along_axis.parallel_apply_along_axis(func1d=function.slope_minimum, arr=arr, axis=0, cores=mp.cpu_count())
+    result = arr
+    #dtype = type(result[0][0])
+    export_arr.cleaned_out_array(outname=outname, arr=result, input_file=input_raster, dtype="float32")
 
     end_time = datetime.now()
     print("end-time = ", end_time-start_time, "Hr:min:sec")
