9f61c84c8b25ad4eccc900ca4abd00e19bf76268
jziemer1996 <jz199623@gmail.com>
2020-02-05 19:52:36 +0100

    added filter_functions.py to pivot branch

---
+++ GEO419_South_Africa/filter_functions.py
@@ -0,0 +1,65 @@
+
+def mean_filter5(arr1d):
+    import numpy as np
+    kernel = (1 / 5.0) * np.ones((5))
+    out = np.float32(np.convolve(arr1d, kernel, "valid"))
+    return(out)
+
+def mean_filter9(arr1d):
+    import numpy as np
+    kernel = (1 / 9.0) * np.ones((9))
+    out = np.float32(np.convolve(arr1d, kernel, "valid"))
+    return(out)
+
+
+def median_filter3(arr1d):
+    import scipy.signal as sig
+    out = sig.medfilt(arr1d, kernel_size= 3)
+    return out
+
+
+def median_filter5(arr1d):
+    import scipy.signal as sig
+    out = sig.medfilt(arr1d, kernel_size= 5)
+    return out
+
+
+def median_filter9(arr1d):
+    import scipy.signal as sig
+    out = sig.medfilt(arr1d, kernel_size= 9)
+    return out
+
+
+def median_filter11(arr1d):
+    import scipy.signal as sig
+    out = sig.medfilt(arr1d, kernel_size= 11)
+    return out
+
+def simple_edge_detection(arr1d):
+    import numpy as np
+    kernel = [-5, -5, -5, -5, 0, 5, 5, 5, 5]
+    out = np.float32(np.convolve(arr1d, kernel, "valid"))
+    if max(out) >= 30:
+        return 1
+    else:
+        return 0
+
+
+def simple_edge_detection9(arr1d):
+    import numpy as np
+    kernel = [-5, -5, -5, -5, 0, 5, 5, 5, 5]
+    out = np.float32(np.convolve(arr1d, kernel, "valid"))
+    return out
+
+
+def simple_edge_detection5(arr1d):
+    import numpy as np
+    kernel = [-5, -5, 0, 5, 5]
+    out = np.float32(np.convolve(arr1d, kernel, "valid"))
+    return out
+
+# empty function only to display -99-free graph in qgis
+def trick(arr1d):
+    import numpy as np
+    out = arr1d
+    return out
