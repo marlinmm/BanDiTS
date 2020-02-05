3c2de35979d87b013231a1eb918315ee6826dcbe
jziemer1996 <jz199623@gmail.com>
2020-02-05 20:24:43 +0100

    configured scripts

--- GEO419_South_Africa/function.py
+++ GEO419_South_Africa/function.py
@@ -2,11 +2,6 @@ import numpy as np
 
 
 # func1d: functions to be applied on 1D array
-def quantile(arr1d, percentile=0.5):
-    import numpy as np
-    return np.percentile(arr1d, percentile)
-
-
 def minimum(arr1d):
     import numpy as np
     return np.min(arr1d)
@@ -47,8 +42,8 @@ def threshold(arr1d):
 def combined(arr1d):
     import numpy as np
     diff = np.max(arr1d) - np.min(arr1d)
-    sigma = np.mean(arr1d) - 3 * np.std(arr1d)
-    if diff >= 6:
+    sigma = np.mean(arr1d) - 2 * np.std(arr1d)
+    if diff >= 5:
         if np.min(arr1d) < sigma:
             return 1
         else:
@@ -56,13 +51,31 @@ def combined(arr1d):
     else:
         return 0
 
+def combined_density(arr1d):
+    import numpy as np
+    diff = np.max(arr1d) - np.min(arr1d)
+    sigma25 = np.mean(arr1d) - 2.5 * np.std(arr1d)
+    sigma3 = np.mean(arr1d) - 3 * np.std(arr1d)
+    sigma4 = np.mean(arr1d) - 4 * np.std(arr1d)
+    if diff >= 5:
+        if np.min(arr1d) < sigma4:
+            return 40
+        if np.min(arr1d) < sigma3:
+            return 30
+        if np.min(arr1d) < sigma25:
+            return 25
+        if np.min(arr1d) >= sigma25:
+            return 0
+    else:
+        return 300
+
 
 def amplitude_if_test(arr1d):
     import numpy as np
     diff = np.max(arr1d) - np.min(arr1d)
-    if diff < 8:
+    if diff < 6:
         return 0
-    if diff >= 8:
+    if diff >= 6:
         return 1
 
 
@@ -91,51 +104,181 @@ def slope_vs_slope(arr1d):
 
     # split time series and list of time series indices in 4 subarrays
     time_series_split = split_list(time_series, wanted_parts=5)
+    #print(time_series_split[0])
     time_series_index_split = split_list(time_series_index, wanted_parts=5)
-    slope_list = []
+    #print(time_series_index_split[0])
 
     # calculate linear regression for each time series subarray
+    slope_list = []
     for i in range (0,len(time_series_index_split)):
         slope, intercept, r_value, p_value, std_err = stats.linregress(time_series_split[i],time_series_index_split[i])
         i +=1
         slope_list = [slope_list, slope]        # weird list append, cause .append doesnt work with multiprocessing
 
     # check for dropping slope values from one fifth of time series to next
-    x = 0
+    temp = 0
     if slope_list[0][0][0][0][1] > 0 and slope_list[0][0][0][1] < 0:
         if slope_list[0][0][0][0][1] - slope_list[0][0][0][1] > 3:
-            x = x + 1
+            temp = temp + 1
         else:
-            x = 2
+            temp = 2
     if slope_list[0][0][0][1] > 0 and slope_list[0][0][1] < 0:
         if slope_list[0][0][0][1] - slope_list[0][0][1] > 3:
-            x = x + 305
+            temp = temp + 305
         else:
-            x = 400
+            temp = 400
 
     if slope_list[0][0][1] > 0 and slope_list[0][1] < 0:
         if slope_list[0][0][1] - slope_list[0][1] > 3:
-            x = x + 500
+            temp = temp + 500
         else:
-            x = x + 600
+            temp = temp + 600
 
     if slope_list[0][1] > 0 and slope_list[1] < 0:
         if slope_list[0][1] - slope_list[1] > 3:
-            x = x + 700
+            temp = temp + 700
         else:
-            x = x + 800
-    # else:
-    #     x = 100
-    return x
+            temp = temp + 800
+    return temp
 
 
-def percentile_year(arr1d):
+def stdev_time(arr1d):
     import numpy as np
-    # do stuff here
-    np.percentile(arr1d)
+    from scipy import stats
+    time_series = arr1d
+    arr_shape = arr1d.shape[0]
+    time_series_index = np.indices((arr_shape,))[0]
+
+    # internal function to split time series in n sub time series
+    def split_list(alist, wanted_parts=1):          # based on: https://stackoverflow.com/a/752562
+        length = len(alist)
+        return [alist[i * length // wanted_parts: (i + 1) * length // wanted_parts]
+                for i in range(wanted_parts)]
+
+    # split time series and list of time series indices in 4 subarrays
+    time_series_split = split_list(time_series, wanted_parts=5)
+    #print(time_series_split[0])
+    #print("sdsdasdasd")
+    time_series_index_split = split_list(time_series_index, wanted_parts=5)
+
+    # calculate linear regression for each time series subarray
+    mini_list = []
+    sigma_list = []
+    for i in range (0,len(time_series_index_split)):
+        mea = np.mean(time_series_split[i])
+        std_mea = 2.5*np.std(time_series_split[i])
+        mini = min(time_series_split[i])
+        sigma = mea - std_mea
+        i +=1
+        mini_list = [mini_list, mini]
+        sigma_list = [sigma_list, sigma]        # weird list append, cause .append doesnt work with multiprocessing
+
+    # check for dropping slope values from one fifth of time series to next
+    temp = 0
+    # if mini_list[0][0][0][0][1] < sigma_list[0][0][0][0][1]:
+    #   temp = temp + 15
+    # if mini_list[0][0][0][1] < sigma_list[0][0][0][1]:
+    #   temp = temp + 16
+    if mini_list[0][0][1] < sigma_list[0][0][1]:
+      temp = temp + 17
+    if mini_list[0][1] < sigma_list[0][1]:
+      temp = temp + 18
+    if mini_list[1] < sigma_list[1]:
+      temp = temp + 19
+    if temp == 0:
+       return 0
+    return temp
+
+def amplitude_time(arr1d):
+        import numpy as np
+        from scipy import stats
+        time_series = arr1d
+        arr_shape = arr1d.shape[0]
+        time_series_index = np.indices((arr_shape,))[0]
+
+        # internal function to split time series in n sub time series
+        def split_list(alist, wanted_parts=1):  # based on: https://stackoverflow.com/a/752562
+            length = len(alist)
+            return [alist[i * length // wanted_parts: (i + 1) * length // wanted_parts]
+                    for i in range(wanted_parts)]
+
+        # split time series and list of time series indices in 4 subarrays
+        time_series_split = split_list(time_series, wanted_parts=5)
+        # print(time_series_split[0])
+        # print("sdsdasdasd")
+        time_series_index_split = split_list(time_series_index, wanted_parts=5)
+
+        # calculate linear regression for each time series subarray
+        diff_list = []
+        for i in range(0, len(time_series_index_split)):
+            maxi = max(time_series_split[i])
+            mini = min(time_series_split[i])
+            diff = maxi - mini
+            i += 1
+
+            diff_list = [diff_list, diff]  # weird list append, cause .append doesnt work with multiprocessing
+
+        # check for dropping slope values from one fifth of time series to next
+        temp = 0
+        # if diff_list[0][0][0][0][1] > 6:
+        #   temp = temp + 15
+        # if diff_list[0][0][0][1] > 6:
+        #     temp = temp + 16
+        if diff_list[0][0][1] > 5:
+            temp = temp + 17
+        if diff_list[0][1] > 5:
+            temp = temp + 18
+        if diff_list[1] > 5:
+            temp = temp + 19
+        if temp == 0:
+            return 0
+        return temp
+
+def find_peaks(arr1d):
+    from scipy.signal import find_peaks
+    peaks = find_peaks(arr1d, height=35)
+    if len(peaks[0]) >= 2 or len(peaks[0]) == 0:
+        return 0
+    if len(peaks[0]) == 1:
+        return 1
+        # print(peaks)
+        # print(peaks[0])
+
+
+
+def find_peaks2(arr1d):
+    from scipy.signal import find_peaks
+    peaks = find_peaks(arr1d, height=30)
+    if len(peaks[0]) >= 3:
+        return 3
+    if len(peaks[0]) >= 2:
+        return 2
+    if len(peaks[0]) >= 1:
+        return 1
+    if len(peaks[0]) < 1:
+        return 0
+
+
+def percentile(arr1d):
+    import numpy as np
+    upper = np.percentile(arr1d, 99)
+    lower = np.percentile(arr1d, 1)
+    return upper - lower
+
     #https://docs.scipy.org/doc/numpy/reference/generated/numpy.percentile.html
+    # def quantile(arr1d, percentile=90):
+    #     import numpy as np
+    #     return np.percentile(arr1d, percentile)
 
 
-# recurrence matrix
+
+# Recurrence matrix:
 # https://pypi.org/project/PyRQA/
 # http://www.pik-potsdam.de/~donges/pyunicorn/api/timeseries/recurrence_plot.html
+
+
+# Convolution -> for use in filtering
+# http://juanreyero.com/article/python/python-convolution.html
+# https://stackoverflow.com/questions/20036663/understanding-numpys-convolve
+
+
--- GEO419_South_Africa/mask_raster.py
+++ GEO419_South_Africa/mask_raster.py
@@ -1,21 +1,134 @@
 import fiona
 import rasterio as rio
 import rasterio.mask
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+from GEO419_South_Africa import preprocessing
 from GEO419_South_Africa import export_arr
 
 
-def mask_raster_test(outname):
-    shapefile = fiona.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Shapes/fire_feb_2017_subset_reproj.shp", "r")
+# def mask_raster_test(outname):
+#     shapefile = fiona.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Shapes/fire_feb_2017_subset_reproj.shp", "r")
+#     shapes = [feature["geometry"] for feature in shapefile]
+#     for i in range(0, len(shapes)):
+#         with rio.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif") as src:
+#             out_image, out_transform = rasterio.mask.mask(src, [shapes[i]], crop=True)
+#             ras_meta = src.profile
+#             ras_meta.update({"driver": "GTiff",
+#                              "height": out_image.shape[1],
+#                              "width": out_image.shape[2],
+#                              "transform": out_transform})
+#             # export_arr.out_array(outname=outname + i, arr=out_image, input_file="C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif", dtype="float32")
+#             with rio.open(outname, 'w', **ras_meta) as dst:
+#                 dst.write(out_image, 1)
+#     #return out_image
+
+list_list = []
+def mask_raster_test():
+    import numpy as np
+    # shapefile = fiona.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Shapes/pivot_test_reproj.shp", "r")
+    shapefile = fiona.open("C:/Users/jz199/Documents/Studium/Master/1. Semester/Vorlesungsmitschriften/GEO419 - Pythonprogrammierung Habermeyer/GEO402_Shapes/pivot.shp", "r")
     shapes = [feature["geometry"] for feature in shapefile]
+
     for i in range(0, len(shapes)):
-        with rio.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif") as src:
-            out_image, out_transform = rasterio.mask.mask(src, [shapes[i]], crop=True)
-            ras_meta = src.profile
-            ras_meta.update({"driver": "GTiff",
-                             "height": out_image.shape[1],
-                             "width": out_image.shape[2],
-                             "transform": out_transform})
-            # export_arr.out_array(outname=outname + i, arr=out_image, input_file="C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif", dtype="float32")
-            with rio.open(outname, 'w', **ras_meta) as dst:
-                dst.write(out_image, 1)
+        # src = rio.open("C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1_A_VH_agulhas_full_study_site_50mcleaned.tif")
+        src = rio.open("C:/Users/jz199/Documents/Studium/Master/1. Semester/Vorlesungsmitschriften/GEO402 - Ableitung von Landoberflächenparametern/Subset/SubsetVH.tif")
+        out_image, out_transform = rasterio.mask.mask(src, [shapes[i]], crop=True, nodata= np.nan)
+        ras_meta = src.profile
+        ras_meta.update({"driver": "GTiff",
+                         "height": out_image.shape[1],
+                         "width": out_image.shape[2],
+                         "transform": out_transform,
+                         "nodata": 0. })
+
+
+        list1 = []
+        for j in range(0, len(out_image)):
+            tmp = np.nanmean(out_image[j])
+            list1.append(tmp)
+
+
+        med_filter = sig.medfilt(list1, kernel_size=3)
+
+
+        # kernel = [-5, -5, -5, -5, 0, 5, 5, 5, 5]
+        kernel = [-5, -5, 0, 5, 5]
+        #print(kernel)
+        out = np.float32(np.convolve(med_filter, kernel, "valid"))
+
+
+        from scipy.signal import find_peaks
+        peaks = find_peaks(out, height=30)
+        #print(peaks)
+        print(peaks[0])
+        #print(peaks[1])
+
+        # median_value = np.median(out)
+        # mean_value = np.mean(out)
+        #
+        # data1 = list1
+        # data2 = med_filter
+        # data3 = out
+        # data4 = [median_value] * len(data3)
+        # data5 = [mean_value] * len(data3)
+        #
+        #
+        #
+        # fig, ax1 = plt.subplots()
+        #
+        # color = '#bebebe'
+        # ax1.set_xlabel('no. of values')
+        # ax1.plot(data1, color=color, linewidth=1)
+        #
+        # color = 'tab:red'
+        # ax1.set_ylabel('median', color=color)  # we already handled the x-label with ax1
+        # ax1.plot(data2, color=color)
+        # ax1.tick_params(axis='y', labelcolor=color)
+        #
+        # ax3 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+        #
+        # color = 'tab:green'
+        # ax3.set_ylabel('Edge detection', color=color)  # we already handled the x-label with ax1
+        # ax3.plot(data3, color=color)
+        # ax3.tick_params(axis='y', labelcolor=color)
+        #
+        # color = 'tab:blue'
+        # ax3.plot(data4, color=color)
+        #
+        # color = '#ffff00'
+        # ax3.plot(data5, color=color)
+        #
+        # fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        # plt.show()
+
+        # fig, ax1 = plt.subplots()
+        # # plt.plot(out)
+        # ax1.set_ylabel("db" + str(i))
+        # plt.plot(list1)
+        # plt.show()
+
+
+
+
+
+        # with open("C:/Users/marli/Desktop/GEO402_Testdaten/AAA_output/test" + str(i) + ".txt", 'w', newline='') as myfile:
+        #     #wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        #     wr = csv.writer(myfile, delimiter=',')
+        #     wr.writerow(list1)
+
+        #list_list.append(list1)
+    #print(list_list)
+
+        #with rio.open("C:/Users/marli/Desktop/GEO402_Testdaten/AAA_output/test.tif", 'w', **ras_meta) as dst:
+        #    dst.write(out_image)
+
+        # export_arr.out_array(outname=outname + i, arr=out_image, input_file="C:/Users/marli/Desktop/GEO402_Testdaten/Input_Files/Raster/S1A_VH_Agulhas_50m_selected_bands_VH_subset2.tif", dtype="float32")
+        # with rio.open(outname, 'w', **ras_meta) as dst:
+        #     dst.write(out_image, 1)
     #return out_image
+
+
+#### activate for testing this file standalone ####
+mask_raster_test()
