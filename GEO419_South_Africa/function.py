b833fb2cc35910ad93307a90179faf75eade8802
marlinmm <marlinmueller.mm@gmail.com>
2020-02-09 18:07:33 +0100

    added docstrings to multiple functions

--- GEO419_South_Africa/function.py
+++ GEO419_South_Africa/function.py
@@ -1,45 +1,114 @@
-import numpy as np
-
-
 # func1d: functions to be applied on 1D array
 def minimum(arr1d):
+    """
+    calculates the minimum of the time series for one pixel
+    ----------
+    arr1d: numpy.array
+        1D array representing the time series for one pixel
+
+    Returns
+    -------
+    numpy.float64
+        minimum of the time series for one pixel
+    """
     import numpy as np
     return np.min(arr1d)
 
 
 def maximum(arr1d):
+    """
+    calculates the maximum of the time series for one pixel
+    ----------
+    arr1d: numpy.array
+        1D array representing the time series for one pixel
+
+    Returns
+    -------
+    numpy.float64
+        maximum of the time series for one pixel
+    """
     import numpy as np
     return np.max(arr1d)
 
 
 def mean(arr1d):
+    """
+    calculates the mean of the time series for one pixel
+    ----------
+    arr1d: numpy.array
+        1D array representing the time series for one pixel
+
+    Returns
+    -------
+    numpy.float64
+        mean of the time series for one pixel
+    """
     import numpy as np
     return np.mean(arr1d)
 
 
 def stdev(arr1d):
+    """
+    calculates the standard deviation of the time series for one pixel
+    ----------
+    arr1d: numpy.array
+        1D array representing the time series for one pixel
+
+    Returns
+    -------
+    numpy.float64
+        standard deviation of the time series for one pixel
+    """
     import numpy as np
     return np.std(arr1d)
 
 
 def improved_stdev(arr1d):
+    """
+    calculates the 95% probability space of each time series and checks if the minimum value falls within the
+    95% intervall or not
+    ----------
+    arr1d: numpy.array
+        1D array representing the time series for one pixel
+
+    Returns
+    -------
+    numpy.int32
+        returns either 1, if the minimum value is lower than the mean values minus two times the standard deviation
+        or returns 0 if this is not the case
+    """
     import numpy as np
-    diff1 = np.mean(arr1d)-2.5*np.std(arr1d)
+    diff1 = np.mean(arr1d)-2*np.std(arr1d)
     if np.min(arr1d) < diff1:
         return 1
     else:
         return 0
 
 
-def threshold(arr1d):
+def threshold(arr1d, threshold):
+    """
+    simple threshold function, which checks, if the minimum value of the time series falls below a certain threshold
+    ----------
+    arr1d: numpy.array
+        1D array representing the time series for one pixel
+    threshold: int
+        should be set between -15 and -25 for best results depending on use case
+
+    Returns
+    -------
+    numpy.int32
+        returns either 1, if the minimum value is lower than the set threshold or returns 0 if this is not the case
+    """
     import numpy as np
-    if np.min(arr1d) < -21:
+    if np.min(arr1d) < threshold:
         return 1
     else:
         return 0
 
 
 def combined(arr1d):
+    ### ADD DOCSTRING ###
+
     import numpy as np
     diff = np.max(arr1d) - np.min(arr1d)
     sigma = np.mean(arr1d) - 2 * np.std(arr1d)
@@ -53,6 +122,19 @@ def combined(arr1d):
 
 
 def amplitude_if_test(arr1d):
+    """
+    calculates the amplitude of the time series and applies a threshold for the amplitude
+    ----------
+    arr1d: numpy.array
+        1D array representing the time series for one pixel
+    threshold: int
+        should be set between 4 and 10 for best results depending on use case
+
+    Returns
+    -------
+    numpy.int32
+        returns either 1, if the amplitude value is higher than the set threshold or returns 0 if this is not the case
+    """
     import numpy as np
     diff = np.max(arr1d) - np.min(arr1d)
     if diff < 6:
@@ -62,6 +144,17 @@ def amplitude_if_test(arr1d):
 
 
 def slope(arr1d):
+    """
+    calculates the slope for the whole time series for one pixel
+    ----------
+    arr1d: numpy.array
+        1D array representing the time series for one pixel
+
+    Returns
+    -------
+    numpy.float64
+        returns slope value of the time series for one pixel
+    """
     import numpy as np
     from scipy import stats
     x = arr1d
@@ -72,6 +165,40 @@ def slope(arr1d):
 
 
 def slope_vs_slope(arr1d):
+    """
+    calculates the slopes for subsections of the time series for one pixel and compares the negative slope change from
+    time n-n and n-n+1, the n-n+1 and n-n+2 until n-1 and n. During this process a value for each result is created
+    allowing the user to extract info on a slope change at a specific time, e.g. the value 1 means a negative slope
+    change between section 1 and 2 of the time series, the value 501 means a negative slope change between section 1
+    and 2 and also a negative slope change between section 3 and 4. End values containing the numbers 2, 400, 600 and
+    800 mean that there was no negative slope change detected in the corresponding time frame
+    ----------
+    arr1d: numpy.array
+        1D array representing the time series for one pixel
+
+    Returns
+    -------
+    numpy.int32
+        0 = no negative slope change detected
+        1 = negative slope change between time frame 1 and 2
+        2 = see 0
+        305 = negative slope change between time frame 2 and 3
+        400 = see 0
+        500 = negative slope change between time frame 3 and 4
+        501 = negative slope change between time frame 3 and 4 and time frame 1 and 2
+        502 = see 500
+        600 = see 0
+        601 = see 1
+        602 = see 0
+        700 = negative slope change between time frame 4 and 5
+        701 = negative slope change between time frame 4 and 5 and time frame 1 and 2
+        702 = see 700
+        800 = see 0
+        1005 = negative slope change between time frame 4 and 5 and time frame 2 and 3
+        1100 = see 700
+        1105 = see 305
+        1200 = see 0
+    """
     import numpy as np
     from scipy import stats
     time_series = arr1d
@@ -123,6 +250,8 @@ def slope_vs_slope(arr1d):
 
 
 def combined_time(arr1d):
+    ### ADD DOCSTRING ###
+
     import numpy as np
     time_series = arr1d
     arr_shape = arr1d.shape[0]
@@ -167,9 +296,24 @@ def combined_time(arr1d):
     return temp
     ### NOT WORKING PROPERLY ###
 
-def find_peaks(arr1d):
+def find_peaks(arr1d, threshold):
+    """
+    !!! STACK NEEDS TO BE MEDIAN- AND SOBEL-FILTERED BEFORE USE OF THIS FUNCTION (see filter_functions.py)!!!
+    finds peaks greater than set height in median- and sobel-filtered time series for each pixel if there is only one
+    peak in the time series
+    ----------
+    arr1d: numpy.array
+        1D array representing the time series for one pixel
+    threshold: int
+        should be set between 20 and 50 for best results
+
+    Returns
+    -------
+    numpy.int32
+        returns either 1, if the time series contains one and only one peak higher than set threshold, otherwise 0
+    """
     from scipy.signal import find_peaks
-    peaks = find_peaks(arr1d, height=35)
+    peaks = find_peaks(arr1d, height=threshold)
     if len(peaks[0]) >= 2 or len(peaks[0]) == 0:
         return 0
     if len(peaks[0]) == 1:
